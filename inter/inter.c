405fb0ceb7289aeebe680fb6114107282f642716
Juanrd10 <60832520+juanrd10@users.noreply.github.com>
2020-07-29 13:04:21 +0200

    Add files via upload

--- inter/inter.c
+++ inter/inter.c
@@ -1,31 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juanrodr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/29 13:01:54 by juanrodr          #+#    #+#             */
+/*   Updated: 2020/07/29 13:02:59 by juanrodr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	int ascii[255] = {0};
-	int move_arg;
-	int move_char;
+	int		ascii[255] = {0};
+	int		move_arg;
+	int		move_char;
 
 	if (argc == 3)
 	{
 		move_arg = 2;
-		while(move_arg > 0)
+		while (move_arg > 0)
 		{
 			move_char = 0;
-			while(argv[move_arg][move_char])
+			while (argv[move_arg][move_char])
 			{
 				if (move_arg == 2 && ascii[(unsigned char)argv[move_arg][move_char]] == 0)
-						ascii[(unsigned char)argv[move_arg][move_char]] = 1;
+					ascii[(unsigned char)argv[move_arg][move_char]] = 1;
 				else if (move_arg == 1 && ascii[(unsigned char)argv[move_arg][move_char]] == 1)
-						{
-						write(1, &argv[move_arg][move_char], 1);
-						ascii[(unsigned char)argv[move_arg][move_char]] = 2;
-						}
+				{
+					write(1, &argv[move_arg][move_char], 1);
+					ascii[(unsigned char)argv[move_arg][move_char]] = 2;
+				}
 				move_char++;
 			}
 			move_arg--;
 		}
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
